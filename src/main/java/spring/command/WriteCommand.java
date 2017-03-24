0f7ee9f4b19d62607625f57995d2cd0d7a82c3fc
B_Chan <lucete1104@gmail.com>
2017-03-24 18:26:44 +0900

    BitChan

---
+++ src/main/java/spring/command/WriteCommand.java
@@ -0,0 +1,42 @@
+package spring.command;
+
+public class WriteCommand {
+	
+	private String userName;
+	private String password;
+	private String subject;
+	private String email;
+	private String content;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
